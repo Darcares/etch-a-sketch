e7efc60ded869c8b931f2d175f87df8f2a2563bf
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2024-12-10 19:09:13 +0000

    Draw horizontal containers

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 const container = document.querySelector(`#container`);
+const containerHeight = 600;
+const containerWidth = 600;
+const dimensions = 16;
+for(let i = 0; i < dimensions; i++) {
+    drawHorizontalContainer(i);
+/*  const gridElement = document.createElement(`div`);
+    gridElement.classList.toggle(`gridElement`);
+    gridElement.style.height = `${containerHeight / dimensions}px`;
+    gridElement.style.width = `${containerWidth / dimensions}px`;
+    gridElement.textContent = `${i + 1}`;
+    container.appendChild(gridElement); */
+}
 
+function drawHorizontalContainer(i) {
+    const horizontalContainer = document.createElement(`div`);
+    horizontalContainer.classList.toggle(`horizontal-container`);
+    horizontalContainer.textContent = `${i + 1}`;
+    container.appendChild(horizontalContainer);
+}
+
+/* function drawVerticalContainer(i) {
+    const verticalContainer = document.createElement(`div`);
+    verticalContainer.classList.toggle(`vertical-container`);
+    verticalContainer.textContent = `${i + 1}`;
+    container.appendChild(verticalContainer);
+} */
