ccaaa5822567f52b4f748c6cb73bfbe736960130
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2024-12-11 13:47:35 +0000

    Refactor functions and variables's names

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 const container = document.querySelector(`#container`);
-const containerHeight = 600;
-const containerWidth = 600;
-const dimensions = 16;
-for(let i = 0; i < dimensions; i++) {
-    drawHorizontalContainer(i);
+const size = 16;
 
-    for(let j = 0; j < dimensions; j++) {
-        drawVerticalContainer(i, j);
+for(let rowNo = 1; rowNo <= size; rowNo++) {
+    drawRow(rowNo);
+
+    for(let columnNo = 1; columnNo <= size; columnNo++) {
+        drawColumn(rowNo, columnNo);
     }
 
 }
 
-function drawHorizontalContainer(i) {
-    const horizontalContainer = document.createElement(`div`);
-    horizontalContainer.classList.toggle(`horizontal-container`);
-    horizontalContainer.setAttribute(`id`, `horizontal-${i + 1}`)  ;
-    container.appendChild(horizontalContainer);
+function drawRow(rowNo) {
+    const row = document.createElement(`div`);
+    row.classList.toggle(`row`);
+    row.setAttribute(`id`, `row-${rowNo}`)  ;
+    container.appendChild(row);
 }
 
-function drawVerticalContainer(i, j) {
-    const firstRow = 0;
-    const lastColumn = 15;
-    const horizontalContainer = document.querySelector(`#horizontal-${i + 1}`)
-    const verticalContainer = document.createElement(`div`);
-    verticalContainer.classList.toggle(`vertical-container`);
-    if (i === firstRow) verticalContainer.classList.toggle(`border-top`);
-    if (j === lastColumn) verticalContainer.classList.toggle(`border-right`);
-    verticalContainer.setAttribute(`id`, `${i + 1},${j + 1}`)
-    horizontalContainer.appendChild(verticalContainer);
+function drawColumn(rowNo, columnNo) {
+    const firstRow = 1;
+    const lastColumn = 16;
+    const row = document.querySelector(`#row-${rowNo}`);
+    const column = document.createElement(`div`);
+    column.classList.toggle(`column`);
+    if(rowNo === firstRow) column.classList.toggle(`border-top`);
+    if(columnNo === lastColumn) column.classList.toggle(`border-right`);
+    column.setAttribute(`id`, `row-${rowNo}, column-${columnNo}`);
+    row.appendChild(column);
 }
